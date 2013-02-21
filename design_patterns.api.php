4720750b453c45575c6fdde40cb36b05b5f39f25
Amarnath Ravikumar <amarnus@Amars-MacBook-Pro.local>
2013-02-22 03:00:26 +0800

    Minor formatting changes that would be expected of any self-respecting Drupal developer.

--- design_patterns.api.php
+++ design_patterns.api.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
- * Allows other modules to respond when Snape performs some action on the Marauder's map.
- *
- * @param string $event
- *   Identifies the action performed by Snape on the Marauder's map.
- *   Can be one of "open_map" or "close_map".
- */
+* Allows other modules to respond when Snape performs some action on the Marauder's map.
+*
+* @param string $event
+*   Identifies the action performed by Snape on the Marauder's map.
+*   Can be one of "open_map" or "close_map".
+*/
 function hook_design_patterns_observer_notify($event) {
-	if ($event == 'open_map') {
-		// Map has been opened.
-	}
-	elseif ($event == 'close_map') {
-		// Map has been closed.
-	}
+  if ($event == 'open_map') {
+    // Map has been opened.
+  }
+  elseif ($event == 'close_map') {
+    // Map has been closed.
+  }
 }
